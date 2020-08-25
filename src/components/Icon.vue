d3c2d9286fcf14c8065df46214a7db589ec61c53
LiMiSu <1102523711@qq.com>
2020-08-25 15:51:27 +0800

    icon

---
+++ src/components/Icon.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg class="icon">
+    <use v-bind:xlink:href="'#'+name"></use>
+  </svg>
+</template>
+
+<script lang="ts">
+  // import label from '@/assets/icons/label.svg';
+  // import money from '@/assets/icons/money.svg';
+  // import statistics from '@/assets/icons/statistics.svg';
+  // console.log(label)
+  // console.log(money)
+  // console.log(statistics)
+
+  //工程化引入
+  //声明一个imporAll方法
+  const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  //使用它
+  try { //单元测试的时候可能会出错，加try catch
+    importAll(require.context('../assets/icons', true, /\.svg$/));
+  } catch (error) {
+    console.log(error);
+  }
+
+  export default {
+    name: 'icon',
+    props:['name']
+  };
+</script>
+
+<style lang="scss" scoped>
+  .icon {
+    width: 1em; height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,31 @@
 <template>
-
   <div class="nav">
-    <router-link to="/money">记账</router-link>|
-    <router-link to="/labels">记一笔</router-link>|
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    |
+    <router-link to="/labels">
+      <Icon name="label"/>
+      记一笔
+    </router-link>
+    |
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
   </div>
-
 </template>
 
 <script lang="ts">
+
   export default {
     name: 'Nav'
   };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
+  .nav {
     border: 1px solid red;
   }
 </style>
