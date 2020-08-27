e2df154a40442c941e7acc1c2fa43d22d6438781
LiMiSu <1102523711@qq.com>
2020-08-27 16:30:20 +0800

    用JS实现支出ä与收入组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -8,7 +8,7 @@
   </layout>
 </template>
 
-<script lang="ts">
+<script>
   import NumberPad from '@/components/Money/NumberPad.vue';
   import InputNumber from '@/components/Money/InputNumber.vue';
   import Types from '@/components/Money/Types.vue';
