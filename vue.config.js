457675a0846b7f98722659d8d4ce71d7c3369a00
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-01 19:36:35 +0800

    不要-

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/moneybook-website/'
+    ? '/moneybookwebsite/'
     : '/',
   lintOnSave: false,
   chainWebpack: config => {
