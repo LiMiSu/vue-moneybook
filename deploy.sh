d46e670ff6eea561c3bb24389ddd76797ff02a87
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-07 15:54:52 +0800

    修改编辑标签为：编辑支出/收入标签

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@gitee.com:sulimi/moneybookwebsite.git master:gh-pages
+ git push -f git@github.com:sulimi/moneybookwebsite.git master:gh-pages
 
 cd -
